edd1610309d043d89f2f738d2ff5e6c91c8b7e92
Niku <86804501+andreiProfessional@users.noreply.github.com>
2025-04-06 14:49:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+// TASK 2 -> NU MERGE
+const button = document.getElementById("button_descopera");
+const paragraf_istorie = document.getElementById("p_istorie");
+paragraf_istorie.style.backgroundColor = "green";
+
+
+// TASK 3
+// By ID
+document.getElementById("page_top");
+// By class
+let elements = document.getElementsByClassName("logo_container");
+// By CSS selector
+let header = document.querySelector("header");
+
+
+
+// TASK 4
+
+
+
+// TASK 5
+
+
+
+// TASK 6
+
+
+
+// TASK 7
+
+
+
+// TASK 8
+
+
+
+// TASK 9
+
+
+
+// TASK 10
+
+
+
+// TASK 11
+
+
+
+// TASK 12
+
+
+
+// TASK 13
+
+
+
+// TASK 14
+
+
+
+// TASK 15
+
+
+// TASK 16
+
+
+
+// TASK 17
